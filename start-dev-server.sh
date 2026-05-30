8ce6b34e7056f8effb21699331e729e0cd78be04
Mistlight <mistlight@otakukingdom.com>
2026-05-30 12:50:30 -0700

    rm gitignore things

--- start-dev-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export DATA_DIR="$SCRIPT_DIR/.grant-ops-data"
-cd "$SCRIPT_DIR/frontend"
-"$SCRIPT_DIR/node_modules/.bin/next" dev -p 3000
